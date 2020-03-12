e1dfc975d62580e308ba49eed83ceea17c3d09d0
qi xiangdong <sulayman.qi@wiya.com.my>
2020-03-12 13:06:23 +0800

    Updates

---
+++ precache-manifest.6d57fec3547428b8dee1628eaa96decc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbb735d4d471bf7573df547216d258e",
+    "url": "/myconora/index.html"
+  },
+  {
+    "revision": "384602264db3741c95cf",
+    "url": "/myconora/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "1b69d1c61b5ba4f95459",
+    "url": "/myconora/static/css/main.2d51ff4f.chunk.css"
+  },
+  {
+    "revision": "384602264db3741c95cf",
+    "url": "/myconora/static/js/2.0eb61d06.chunk.js"
+  },
+  {
+    "revision": "1f0a7691723b8d205c0bd83f00663892",
+    "url": "/myconora/static/js/2.0eb61d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b69d1c61b5ba4f95459",
+    "url": "/myconora/static/js/main.3e02ef8f.chunk.js"
+  },
+  {
+    "revision": "404e10d1572ac08d04a5",
+    "url": "/myconora/static/js/runtime-main.e978349e.js"
+  }
+]);
